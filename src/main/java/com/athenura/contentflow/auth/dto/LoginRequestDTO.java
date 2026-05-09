e79d4badf43852ea79fa2667749360358e7d43ee
Stavan Dalal <dalalstavan7@gmail.com>
2026-05-09 17:55:20 +0530

    Implemented user register with Spring Security

--- src/main/java/com/athenura/contentflow/auth/dto/LoginRequestDTO.java
+++ src/main/java/com/athenura/contentflow/auth/dto/LoginRequestDTO.java
@@ -1,5 +1,14 @@
 package com.athenura.contentflow.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
 
     String email;
--- src/main/java/com/athenura/contentflow/auth/dto/RegistrationRequestDTO.java
+++ src/main/java/com/athenura/contentflow/auth/dto/RegistrationRequestDTO.java
@@ -1,5 +1,15 @@
 package com.athenura.contentflow.auth.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegistrationRequestDTO {
     String name;
     String email;
